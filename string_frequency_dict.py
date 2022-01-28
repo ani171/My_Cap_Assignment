98916b19a909c46f86604b255df6692ed0921201
Anirudh N S <anirudh04027@gmail.com>
2022-01-28 17:20:34 +0530

    Update string_frequency_dict.py

--- string_frequency_dict.py
+++ string_frequency_dict.py
@@ -1,12 +1,21 @@
-most_freq(n)'''
 def most_freq(x):
-    d = dict()
-    for key in x:
-        if key not in d:
-            d[key] = 1
-        else:
-            d[key] += 1
-    return d
-string=input("Enter a string:")
-print(most_freq(string))
+    d={}
+    for i in x:
+        d[i]=x.count(i)
+    l=[]
+    for j in d.values():
+        l.append(j)
+    l.sort()
+    l.reverse()
+    print(l)
+    l1=[]
+    for k in l:
+        for a,b in d.items():
+            if b==k:
+                if a not in l1:
+                    l1.append(a)
+    for o in l1:
+        print(o,end="")
 
+n=input("Enter string")
+most_freq(n)
